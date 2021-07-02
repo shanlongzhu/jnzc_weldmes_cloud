8103ed5f5e9ce9a32bc5264dc7f28c679eec3fb5
Admin <kf@WELDMES.com>
2021-07-02 09:35:23 +0800

    班组生产数据报表来源修改

---
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/weldStatisticsData.java
@@ -0,0 +1,55 @@
+package com.gw.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor//生成一个无参构造函数
+@Data
+@Accessors(chain=true)
+public class weldStatisticsData implements Serializable {
+    private Long id;
+    private Integer weldModel;
+    private String gatherNo;
+    private String welderNo;
+    private Integer weldStatus;
+    private Long weldDuration;
+    private Long machineId;
+    private String machineNo;
+    private Long machineDeptId;
+    private Long gatherId;
+    private Long gatherDeptId;
+    private Long welderId;
+    private String welderName;
+    private Long welderDeptId;
+    private Long taskId;
+    private String taskName;
+    private String taskNo;
+    private String createTime;
+    private String startTime;
+    private String endTime;
+    private SysDept sysDept;
+    private MachineWeldInfo machineWeldInfo;
+    private Integer count;
+    private Integer count2;
+    private Integer count3;
+    private Integer count4;
+    private Integer count5;
+    private BigDecimal utilization;
+    @JsonFormat(pattern="HH:mm:ss",timezone="GMT+8")
+    private Timestamp time;
+    @JsonFormat(pattern="HH:mm:ss",timezone="GMT+8")
+    private Timestamp time2;
+    private BigDecimal utilization2;
+    private WelderInfo welderInfo;
+    private TaskInfo taskInfo;
+    private SysDictionary sysDictionary;
+}
