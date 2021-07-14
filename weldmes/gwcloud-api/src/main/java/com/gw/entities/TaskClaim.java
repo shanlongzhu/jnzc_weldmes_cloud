ab06b1dce6831df94315707f7ee00c1f925f5f95
zhanghan <851949632@qq.com>
2021-07-14 19:24:30 +0800

    代码优化代码提交

--- weldmes/gwcloud-api/src/main/java/com/gw/entities/TaskClaim.java
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/TaskClaim.java
@@ -8,14 +8,29 @@ import java.io.Serializable;
 @Data
 public class TaskClaim implements Serializable {
 
+    /**
+     * 主键
+     */
     private long id;
 
+    /**
+     * 焊工id
+     */
     private Long welderId;
 
+    /**
+     * 任务id
+     */
     private Long taskId;
 
+    /**
+     * 焊机id
+     */
     private Long weldId;
 
+    /**
+     * 领取时间
+     */
     private String claimTime;
 
 
--- weldmes/gwcloud-api/src/main/java/com/gw/entities/TaskInfo.java
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/TaskInfo.java
@@ -15,57 +15,133 @@ import java.sql.Timestamp;
 @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 public class TaskInfo {
 
+    /**
+     * id
+     */
     private long id;
 
+    /**
+     * 任务名称
+     */
     private String taskName;
 
+    /**
+     * 任务编号
+     */
     private String taskNo;
 
+    /**
+     * 计划开始时间
+     */
     private String planStarttime;
 
+    /**
+     * 计划结束时间
+     */
     private String planEndtime;
 
+    /**
+     * 实际开始时间
+     */
     private String realityStarttime;
 
+    /**
+     * 实际结束时间
+     */
     private String realityEndtime;
 
+    /**
+     * 部门id
+     */
     private Long deptId;
 
-    //所属班组
+    /**
+     * 部门名称
+     */
     private String deptName;
 
+    /**
+     * 焊工id
+     */
     private Long welderId;
 
-    //焊工姓名
+    /**
+     * 焊工姓名
+     */
     private String welderName;
 
-    //任务等级
+    /**
+     * 等级字典id
+     */
     private byte grade;
 
-    //任务等级
+    /**
+     * 等级id对应字符串
+     */
     private String gradeIdToStr;
 
-    //评价星级Id
+    /**
+     * 评价星级字典id
+     */
     private byte evaluateStars;
 
-    //评价星级
+    /**
+     * 评价星级字典id对应字符串
+     */
     private String evaluateStarsIdToStr;
 
+    /**
+     * 评价内容
+     */
     private String evaluateContent;
 
-    //任务状态Id
+    /**
+     * 状态字典id
+     */
     private int status;
 
-    //任务状态
+    /**
+     * 状态字典id对应字符串
+     */
     private String statusStr;
 
+    /**
+     * 创建人
+     */
     private String createBy;
 
-    private Timestamp createTime;
+    /**
+     * 创建时间
+     */
+    private String createTime;
 
-    //所属作业区
+    /**
+     * 所属作业区名称
+     */
     private String workSpaceName;
 
-    //所属作业区
+    /**
+     * 所属作业区id
+     */
     private Long workSpaceId;
+
+    /**
+     * 电流上限
+     */
+    private Double eleMax;
+
+    /**
+     * 电流下限
+     */
+    private Double eleMin;
+
+    /**
+     * 电压上限
+     */
+    private Double volMax;
+
+    /**
+     * 电压下限
+     */
+    private Double volMin;
 }
