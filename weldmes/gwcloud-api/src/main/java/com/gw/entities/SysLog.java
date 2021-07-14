b4ce980d495f814d093ae250d50c00167e812619
zhanghan <851949632@qq.com>
2021-07-14 14:42:54 +0800

    实体类提交

--- weldmes/gwcloud-api/src/main/java/com/gw/entities/SysLog.java
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/SysLog.java
@@ -1,45 +1,71 @@
 package com.gw.entities;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
-import java.io.Serializable;
-import java.math.BigInteger;
-import java.sql.Timestamp;
 
 
-@AllArgsConstructor
-@NoArgsConstructor//生成一个无参构造函数
+
+/**
+ * @Author zhanghan
+ * @Date 2021/7/14 10:12
+ * @Description  日志信息实体类
+ * @Params
+ */
 @Data
-@Accessors(chain=true)
-@TableName(value = "sys_log")//指定表名
-public class SysLog implements Serializable {
-    @TableId(type = IdType.AUTO)        //主见注解，数据库ID自增
-    private BigInteger id;
-    @TableField(value = "user_name")
-    private String userName;
-    private String operation;
-    private String method;
-    private String params;
-    private BigInteger time;
-    private String ip;
-    @TableField(value = "create_by")
-    private String createBy;
-    @TableField("create_time")
-    private String createTime;
-    @TableField("last_update_by")
-    private String lastUpdateBy;
-    @TableField("last_update_time")
-    private String lastUpdateTime;
+public class SysLog {
 
-    @TableField(exist = false)
-    private String starttime;
-    @TableField(exist = false)
-    private String endtime;
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 用户操作
+     */
+    private String operation;
+
+    /**
+     * 请求方法
+     */
+    private String method;
+
+    /**
+     * 请求参数
+     */
+    private String params;
+
+    /**
+     * 执行时长(毫秒)
+     */
+    private String time;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    /**
+     * 创建人
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    private String createTime;
+
+    /**
+     * 更新人
+     */
+    private String lastUpdateBy;
+
+    /**
+     * 更新时间
+     */
+    private String lastUpdateTime;
 
 }
