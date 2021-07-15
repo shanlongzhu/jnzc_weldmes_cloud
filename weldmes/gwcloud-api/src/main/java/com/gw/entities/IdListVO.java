3f36b2b899d347cdaf41f7cffe67cc963faff70d
zhanghan <851949632@qq.com>
2021-07-15 19:02:10 +0800

    代码优化提交

--- weldmes/gwcloud-api/src/main/java/com/gw/entities/IdListVO.java
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/IdListVO.java
@@ -16,4 +16,14 @@ public class IdListVO {
      * id列表
      */
     List<Long> idList;
+
+    /**
+     * 区域id列表
+     */
+    List<AreaBayInfo> areaBayInfos;
+
+    /**
+     * 厂商id列表
+     */
+    List<FirmMachineInfo> firmMachineInfos;
 }
