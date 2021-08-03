99a3b67b7ddf0fb1056c9263c3a9ea66e50c4de4
lizhengkai <603453103@qq.com>
2021-08-03 09:07:12 +0800

    生产任务详情代码修改

--- weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/data/weldTask/service/WeldTaskService.java
+++ weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/data/weldTask/service/WeldTaskService.java
@@ -1,9 +1,10 @@
 package com.gw.data.weldTask.service;
 
 import com.gw.entities.RealtimeData;
+import com.gw.entities.WeldStatisticsData;
 
 import java.util.List;
 
 public interface WeldTaskService {
-    List<RealtimeData> getList(Long areaId,Long teamId,String time1, String time2,Long status);
+    List<WeldStatisticsData> getList(Long areaId, Long teamId, String time1, String time2, Long status);
 }
--- weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/data/weldTask/service/impl/WeldTaskServiceImpl.java
+++ weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/data/weldTask/service/impl/WeldTaskServiceImpl.java
@@ -3,6 +3,7 @@ package com.gw.data.weldTask.service.impl;
 import com.gw.data.weldTask.dao.WeldTaskDao;
 import com.gw.data.weldTask.service.WeldTaskService;
 import com.gw.entities.RealtimeData;
+import com.gw.entities.WeldStatisticsData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class WeldTaskServiceImpl implements WeldTaskService {
     private WeldTaskDao weldTaskDao;
 
     @Override
-    public List<RealtimeData> getList(Long areaId, Long teamId, String time1, String time2,Long status) {
+    public List<WeldStatisticsData> getList(Long areaId, Long teamId, String time1, String time2, Long status) {
         String teamName=weldTaskDao.getTeamName(teamId);
         String valueName=weldTaskDao.getValueName(status);
         return weldTaskDao.getList(areaId,teamName,time1,time2,valueName);
