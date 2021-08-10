c4235035c000403700e9619300fc02ebbd7344a4
lizhengkai <603453103@qq.com>
2021-08-10 11:01:32 +0800

    设备生产数据代码修改

--- weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/data/device/dao/DeviceDao.java
+++ weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/data/device/dao/DeviceDao.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 @Mapper
 public interface DeviceDao {
-    List<WeldStatisticsData> getList(@Param("time1") String time1, @Param("time2") String time2);
+    List<WeldStatisticsData> getList(@Param("time1") String time1, @Param("time2") String time2, @Param("machineNo") String machineNo,@Param("name") String name);
+
+    String getName(Long deptId);
 }
