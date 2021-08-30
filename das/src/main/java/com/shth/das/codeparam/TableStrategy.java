5366d1a32327baa40b373e173d8d89322b0a32a0
shanlong <shanlong666>
2021-08-30 14:12:35 +0800

    分表策略执行时间修改

--- das/src/main/java/com/shth/das/codeparam/TableStrategy.java
+++ das/src/main/java/com/shth/das/codeparam/TableStrategy.java
@@ -25,12 +25,29 @@ public class TableStrategy {
      * 按周分表（每周日23点执行）：0 0 23 ? * SUN
      * 按天分表（每天23点执行）：0 0 23 * * ?
      */
-    public static final String EXECUTE_TIME = "0 0 23 * * ?";
+    public static final String EXECUTE_TIME = TableStrategy.DAY_TIME;
 
     /**
      * 指定使用的分表策略
      */
     private static final String TABLE_STRATEGY = TableStrategyEnum.DAY.getStrategy();
+
+    /**
+     * 按年分表（每月底23点执行）：0 0 23 L * ?
+     */
+    private static final String YEAR_TIME = "0 0 23 L * ?";
+    /**
+     * 按月分表（每月底23点执行）：0 0 23 L * ?
+     */
+    private static final String MONTH_TIME = "0 0 23 L * ?";
+    /**
+     * 按周分表（每周日23点执行）：0 0 23 ? * SUN
+     */
+    private static final String WEEK_TIME = "0 0 23 ? * SUN";
+    /**
+     * 按天分表（每天23点执行）：0 0 23 * * ?
+     */
+    private static final String DAY_TIME = "0 0 23 * * ?";
 
     /**
      * OTC表名前缀
