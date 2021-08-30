963b81954091bcd8d374377b7f7813cd8b97a455
zhanghan <851949632@qq.com>
2021-08-30 16:10:24 +0800

    松下AT3列表查询、新增、修改、删除接口提交

---
+++ weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/process/craft/dao/SxAT3ProcessIssueDao.java
@@ -0,0 +1,52 @@
+package com.gw.process.craft.dao;
+
+import com.gw.entities.SxAT3ProcessIssue;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @Author zhanghan
+ * @Date 2021/8/30 14:23
+ * @Description
+ */
+@Mapper
+public interface SxAT3ProcessIssueDao {
+
+    /**
+     * @Date 2021/8/30 14:21
+     * @Description 松下AT3工艺列表查询
+     * @Params  工艺库id
+     */
+    public List<SxAT3ProcessIssue> selectSxAT3ProcessIssueInfos(@Param("wpsLibraryId")Long wpsLibraryId);
+
+    /**
+     * @Date 2021/8/30 14:45
+     * @Description 松下AT3工艺信息新增
+     * @Params
+     */
+    public void insertSxAT3ProcessIssueInfo(@Param("sxAT3ProcessIssue")SxAT3ProcessIssue sxAT3ProcessIssue);
+
+    /**
+     * @Date 2021/8/30 15:08
+     * @Description 根据 id 查询 松下AT3工艺信息
+     * @Params
+     */
+    public SxAT3ProcessIssue selectSxAT3ProcessIssueById(@Param("id")Long id);
+
+    /**
+     * @Date 2021/8/30 15:15
+     * @Description 修改松下AT3工艺信息
+     * @Params
+     */
+    public void updateSxAT3ProcessIssueInfo(@Param("sxAT3ProcessIssue")SxAT3ProcessIssue sxAT3ProcessIssue);
+
+    /**
+     * @Date 2021/8/30 15:26
+     * @Description 删除松下AT3工艺信息
+     * @Params
+     */
+    public void deleteSxAT3ProcessIssueInfo(@Param("id")Long id);
+
+}
---
+++ weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/process/craft/service/SxAT3ProcessIssueService.java
@@ -0,0 +1,48 @@
+package com.gw.process.craft.service;
+
+import com.gw.entities.SxAT3ProcessIssue;
+
+import java.util.List;
+
+/**
+ * @Author zhanghan
+ * @Date 2021/8/30 14:20
+ * @Description 松下AT3工艺业务层
+ */
+public interface SxAT3ProcessIssueService {
+
+    /**
+     * @Date 2021/8/30 14:21
+     * @Description 松下AT3工艺列表查询
+     * @Params  工艺库id
+     */
+    public List<SxAT3ProcessIssue> getSxAT3ProcessIssueInfos(Long wpsLibraryId);
+
+    /**
+     * @Date 2021/8/30 14:44
+     * @Description 松下AT3工艺信息新增
+     * @Params
+     */
+    public void addSxAT3ProcessIssueInfo(SxAT3ProcessIssue sxAT3ProcessIssue);
+
+    /**
+     * @Date 2021/8/30 15:07
+     * @Description 根据 id 查询 松下AT3工艺信息
+     * @Params
+     */
+    public SxAT3ProcessIssue getSxAT3ProcessIssueInfoById(Long id);
+
+    /**
+     * @Date 2021/8/30 15:14
+     * @Description 修改松下AT3工艺信息
+     * @Params
+     */
+    public void updateSxAT3ProcessIssueInfo(SxAT3ProcessIssue sxAT3ProcessIssue);
+
+    /**
+     * @Date 2021/8/30 15:23
+     * @Description 删除松下AT3工艺信息
+     * @Params
+     */
+    public void deleteSxAT3ProcessIssueInfo(Long id);
+}
