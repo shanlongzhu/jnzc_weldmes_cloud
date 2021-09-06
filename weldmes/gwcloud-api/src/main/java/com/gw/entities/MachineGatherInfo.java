81c3d00c772b86662af6ee7c6618250584999f7d
zhanghan <851949632@qq.com>
2021-09-06 15:15:36 +0800

    采集模块实体类注释添加

--- weldmes/gwcloud-api/src/main/java/com/gw/entities/MachineGatherInfo.java
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/MachineGatherInfo.java
@@ -11,19 +11,75 @@ import java.io.Serializable;
 @Data
 @Accessors(chain=true)
 public class MachineGatherInfo implements Serializable {
+
+    /**
+     * 主键
+     */
     private long id;
+
+    /**
+     * 采集编号
+     */
     private String gatherNo;
+
+    /**
+     * 采集盒状态
+     */
     private int status;
+
+    /**
+     * 机构ID
+     */
     private long deptId;
+
+    /**
+     * 生产日期
+     */
     private String productionDate;
+
+    /**
+     * IP地址
+     */
     private String ipPath;
+
+    /**
+     * mac地址
+     */
     private String macPath;
+
+    /**
+     * 协议
+     */
     private int protocol;
+
+    /**
+     * 创建人
+     */
     private String createBy;
+
+    /**
+     * 创建时间
+     */
     private String createTime;
+
+    /**
+     * 更新人
+     */
     private String lastUpdateBy;
+
+    /**
+     * 更新时间
+     */
     private String lastUpdateTime;
+
+    /**
+     * 字典类
+     */
     private SysDictionary sysDictionary;
+
+    /**
+     * 部门类
+     */
     private SysDept sysDept;
 
 }
