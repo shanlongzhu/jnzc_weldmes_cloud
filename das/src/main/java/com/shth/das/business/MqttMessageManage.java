ccbd793993d71c8c1e6f2387e21eafddc8c58e78
shanlong <shanlong666>
2021-09-16 09:54:06 +0800

    MQTT主题命名修改

--- das/src/main/java/com/shth/das/business/MqttMessageManage.java
+++ das/src/main/java/com/shth/das/business/MqttMessageManage.java
@@ -2,6 +2,7 @@ package com.shth.das.business;
 
 import com.alibaba.druid.util.StringUtils;
 import com.shth.das.codeparam.MqttParam;
+import com.shth.das.common.DownTopicEnum;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,29 +24,29 @@ public class MqttMessageManage {
     public MqttMessageManage() {
         MqttMessageAnalysis mqttMessageAnalysis = new MqttMessageAnalysis();
         //焊工刷卡领取任务
-        mqttMessageMap.put("taskClaimIssue", mqttMessageAnalysis::taskClaimIssue);
+        mqttMessageMap.put(DownTopicEnum.jnTaskClaimIssue.name(), mqttMessageAnalysis::taskClaimIssue);
         //OTC（1.0）工艺下发
-        mqttMessageMap.put("processIssue", mqttMessageAnalysis::otcV1ProcessIssue);
+        mqttMessageMap.put(DownTopicEnum.jnOtcV1ProcessIssue.name(), mqttMessageAnalysis::otcV1ProcessIssue);
         //OTC（1.0）工艺索取
-        mqttMessageMap.put("processClaim", mqttMessageAnalysis::otcV1ProcessClaim);
+        mqttMessageMap.put(DownTopicEnum.jnOtcV1ProcessClaim.name(), mqttMessageAnalysis::otcV1ProcessClaim);
         //OTC（1.0）密码下发
-        mqttMessageMap.put("passwordIssue", mqttMessageAnalysis::otcV1PasswordIssue);
+        mqttMessageMap.put(DownTopicEnum.jnOtcV1PasswordIssue.name(), mqttMessageAnalysis::otcV1PasswordIssue);
         //OTC（1.0）控制命令下发
-        mqttMessageMap.put("commandIssue", mqttMessageAnalysis::otcV1CommandIssue);
+        mqttMessageMap.put(DownTopicEnum.jnOtcV1CommandIssue.name(), mqttMessageAnalysis::otcV1CommandIssue);
         //松下GL5系列CO2焊机工艺下发
-        mqttMessageMap.put("sxGl5Co2ProcessIssue", mqttMessageAnalysis::sxGl5Co2ProcessIssue);
+        mqttMessageMap.put(DownTopicEnum.jnSxGl5Co2ProcessIssue.name(), mqttMessageAnalysis::sxGl5Co2ProcessIssue);
         //松下GL5系列TIG焊机工艺下发
-        mqttMessageMap.put("sxGl5TigProcessIssue", mqttMessageAnalysis::sxGl5TigProcessIssue);
+        mqttMessageMap.put(DownTopicEnum.jnSxGl5TigProcessIssue.name(), mqttMessageAnalysis::sxGl5TigProcessIssue);
         //松下GL5系列【通道设定、通道读取】:106
-        mqttMessageMap.put("sxGl5WeldChannelSet", mqttMessageAnalysis::sxGl5WeldChannelSet);
+        mqttMessageMap.put(DownTopicEnum.jnSxGl5WeldChannelSet.name(), mqttMessageAnalysis::sxGl5WeldChannelSet);
         //松下GL5系列【工艺索取、工艺删除】:106
-        mqttMessageMap.put("sxGl5ProcessClaim", mqttMessageAnalysis::sxGl5ProcessClaim);
+        mqttMessageMap.put(DownTopicEnum.jnSxGl5ProcessClaim.name(), mqttMessageAnalysis::sxGl5ProcessClaim);
         //松下【FR2、AT3】系列【通道参数查询、通道参数删除】
-        mqttMessageMap.put("sxFr2ChannelParamQuery", mqttMessageAnalysis::sxFr2ChannelParamQuery);
+        mqttMessageMap.put(DownTopicEnum.jnSxFr2ChannelParamQuery.name(), mqttMessageAnalysis::sxFr2ChannelParamQuery);
         //松下FR2系列【通道参数下载】
-        mqttMessageMap.put("sxFr2ChannelParamDownload", mqttMessageAnalysis::sxFr2ChannelParamDownload);
+        mqttMessageMap.put(DownTopicEnum.jnSxFr2ChannelParamDownload.name(), mqttMessageAnalysis::sxFr2ChannelParamDownload);
         //松下AT3系列【通道参数下载】
-        mqttMessageMap.put("sxAt3ParamDownload", mqttMessageAnalysis::sxAt3ParamDownload);
+        mqttMessageMap.put(DownTopicEnum.jnSxAt3ParamDownload.name(), mqttMessageAnalysis::sxAt3ParamDownload);
     }
 
     /**
