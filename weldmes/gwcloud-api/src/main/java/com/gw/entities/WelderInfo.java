776a2b47fab3fe92ad654c8fd83e34a0502dae99
zhanghan <851949632@qq.com>
2021-09-28 20:23:21 +0800

    焊工实体类修改

--- weldmes/gwcloud-api/src/main/java/com/gw/entities/WelderInfo.java
+++ weldmes/gwcloud-api/src/main/java/com/gw/entities/WelderInfo.java
@@ -1,17 +1,12 @@
 package com.gw.entities;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 import java.io.Serializable;
-import java.sql.Timestamp;
 
 
-@AllArgsConstructor
-@NoArgsConstructor//生成一个无参构造函数
+
 @Data
-@Accessors(chain=true)
 public class WelderInfo implements Serializable {
     private Long id;
     private String welderName;
