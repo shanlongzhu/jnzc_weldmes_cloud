abacf33c03283281a00d745736e8299246238af3
zhanghan <851949632@qq.com>
2021-10-15 10:13:01 +0800

    区域跨间树状图修改

--- weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/equipment/collection/service/impl/AreaBayServiceImpl.java
+++ weldmes/gwcloud-provider-dept-8001/src/main/java/com/gw/equipment/collection/service/impl/AreaBayServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @Author zhanghan
