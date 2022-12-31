bbebb90e34c13143ba7ee4964e828dec1ea59cd5
long <zhushanlong@126.com>
2022-12-31 18:14:38 +0800

    编程式事务

--- das/src/test/java/com/shth/das/DasApplicationTests.java
+++ das/src/test/java/com/shth/das/DasApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -22,6 +23,9 @@ public class DasApplicationTests extends BaseTest {
 
     @Autowired
     MongoTemplate mongoTemplate;
+
+    @Autowired
+    TransactionTemplate transactionTemplate;
 
     @Test
     public void test() {
@@ -58,6 +62,27 @@ public class DasApplicationTests extends BaseTest {
         Query query = new Query();
         List<JNRtDataDB> otc_weld_rtdata = mongoTemplate.find(query, JNRtDataDB.class);
         System.out.println("---size---" + otc_weld_rtdata.size());
+    }
+
+    @Test
+    public void testTransactionTemplate() {
+        transactionTemplate.executeWithoutResult(transactionStatus -> {
+            TaskModel taskModel = new TaskModel();
+            try {
+                taskModel.setTaskName("测试编程式事务");
+                taskModel.setTaskNo("000A");
+                taskModel.setStatus(0);
+                int insert = taskMapper.insert(taskModel);
+                System.out.println("insert:" + insert);
+                System.out.println("taskModel:" + taskModel);
+
+                int i = 1 / 0;
+            } catch (Exception e) {
+                log.error("测试编程式事务异常,手动回滚事务：", e);
+                //手动回滚
+                taskMapper.deleteById(taskModel);
+            }
+        });
     }
 
 }
